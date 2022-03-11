7b37ace5c5ab7230df0c1e8f06c42203700c2850
sultanoss <radwansultan@hotmail.dem>
2022-03-11 10:14:54 +0100

    Added images and started working on slidshowt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'radwansultan';
+export class AppComponent implements OnInit {
+  images = ['autorennen.jpg', 'treff.jpg', 'mottorad1.jpg'];
+  currentImg = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+
+    setInterval(() => {
+      this.currentImg++;
+      this.currentImg = this.currentImg % this.images.length;
+      this.showImage = false;
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
+
 }
